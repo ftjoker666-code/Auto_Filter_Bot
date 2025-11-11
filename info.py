b7dc079b38aac861f7b5c0c28d1aa6a5a3934a0e
ftjoker666-code <ftjoker666@gmail.com>
2025-11-11 20:31:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -122,7 +122,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/L_Dorado666') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/L_Dorado666') # Owner link for the bot
-UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/FtCinema_bot') # Update channel link for the bot
+UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/ft_update') # Update channel link for the bot
 
 # ============================
 # User Configuration
